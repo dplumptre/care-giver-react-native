ff2d795c9314baa5a3f21d1657d6bd90d28f94c2
Ademola <dplumptre@yahoo.com>
2025-06-01 13:52:37 +0100

    logo

--- constant/konst.js
+++ constant/konst.js
@@ -7,7 +7,7 @@
 //export const urlA = 'http://elasticcarerapp-env.eba-5tmkqe9k.eu-central-1.elasticbeanstalk.com/api'; 
 
 //  For Android Emulator (development)
- export const urlA = 'http://10.0.2.2:5000/api';
+ export const urlA = 'http://10.0.2.2:2125/api';
 
 //  For physical device on same Wi-Fi (development)
 // export const urlA = 'http://192.168.1.110/api';
